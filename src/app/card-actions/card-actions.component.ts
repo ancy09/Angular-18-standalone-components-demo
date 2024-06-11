02597206c860f459ab83b071d7b688c34cfcc2a3
Ancy Anto <51624049+ancy09@users.noreply.github.com>
2024-06-11 23:19:49 +0530

    Added card actions component

---
+++ src/app/card-actions/card-actions.component.ts
@@ -0,0 +1,19 @@
+// card-actions.component.ts
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'corp-card-actions',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div class="corp-card-actions">
+      <ng-content></ng-content>
+    </div>
+  `,
+  styleUrls: ['./card-actions.component.css']
+})
+export class CardActionsComponent {
+
+}
