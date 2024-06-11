86e7537b2dea06d6201e6e88c00a40a21671c24f
Ancy Anto <51624049+ancy09@users.noreply.github.com>
2024-06-11 23:23:23 +0530

    Added card component

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,19 @@
+//card.component.ts
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'corp-card',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div class="corp-card">
+        <ng-content></ng-content>
+    </div>
+  `,
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+
+}
